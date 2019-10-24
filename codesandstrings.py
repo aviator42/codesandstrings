01661e35163ef4e1d93a3c622474b1cc5f43084e
aviator42 <migu3l.r4mon@gmail.com>
2019-10-24 15:27:44 -0500

    rowrow

---
+++ codesandstrings.py
@@ -0,0 +1,27 @@
+# Strings
+
+# Concatenation
+#    2 or more strings and put them together
+
+firstName = "Mo"
+lastName = "Lester"
+
+print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + ", " + firstName
+
+print(lastFirst)
+
+# Repetition
+# repetition activator
+
+print("Hip "*2 + "Hooray !")
+
+def rowYourBoat():
+    print("Row, "*3 + "your boat")
+    print("Gently down the stream")
+    print("merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
